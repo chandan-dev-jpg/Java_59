78a4dc653ebbdfe71369e38cfd765bde7502172b
Array in java <chandanbehera4598@gmail.com>
2026-07-09 22:14:00 +0530

    File Update

--- Notes/Opps/Employee.java
+++ Notes/Opps/Employee.java
@@ -1,24 +1,5 @@
-class Test
-{
-    public Test(int x)
+public static void main(String[] args) {
     {
-        this("java");
-        IO.println("int x value is "+x);
-
+        
     }
-    public Test(String y)
-    {
-        this(90.0f);
-        IO.println("String y value is "+y);
-    }
-    public Test(float f)
-    {
-        super();
-        IO.println("Float Value is "+f);
-    }
-}
-
-void main()
-{
-    new Test(16); 
 }
