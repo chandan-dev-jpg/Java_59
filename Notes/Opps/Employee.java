ba773e34c7dc3fb7cef1139bda872b2031b8aa67
Array in java <chandanbehera4598@gmail.com>
2026-07-10 21:28:47 +0530

    pratice constructor

--- Notes/Opps/Employee.java
+++ Notes/Opps/Employee.java
@@ -1,6 +1,34 @@
 void main()
 {
-    byte b=(byte)140;
-    IO.println(b);
+    Dog d=new Dog("raju", 10, 2.3);
+    IO.println(d);
+}
 
+class Dog
+{
+private String name;
+private int age;
+private double height;
+
+public Dog(String name,int age,double height)
+
+{
+    this.name=name;
+    this.age=age;
+    this.height=height;
+
+
+    if(name.equals(null)||name.isBlank()||age<=0||height<=0)
+    {
+IO.println("Invalid Data");
+return;
+    }
+
+}
+
+public String toString()
+{
+    return "Dog [Name"+name+" Age "+age+" height"+height+"]";
+
+}
 }
