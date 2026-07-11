6364a663a4352b4fa0f40b17f300e11d9d1b780a
Array in java <chandanbehera4598@gmail.com>
2026-07-11 19:56:20 +0530

    BlocK

--- Notes/Opps/instanceBlock.java
+++ Notes/Opps/instanceBlock.java
@@ -1,8 +1,33 @@
-void main(){
-
-float f=90.0f;
-long d=20;
- r=f+d;
-
-
+void main()
+{
+new Calculate(100, 200);
+new Calculate(100, 200);
 }
+
+class Calculate
+{
+    public Calculate(int x,int y)
+    {
+        this(1.2,1.2);
+        IO.println("Sum of two intger s"+(x+y));
+    }
+
+    public Calculate(double x,double y)
+    {
+this("java","Technology");
+IO.println("Sum of two double is :"+(x+y));
+    }
+
+    public Calculate(String x,String y)
+    {
+        IO.println("Concatenation of two String is :"+(x+y));
+
+    }
+    {
+        IO.println("Non static");
+    }
+}
+
+
+
+
