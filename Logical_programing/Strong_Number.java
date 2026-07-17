97c209e2e6d370a88a43908dfb11d9d78e0de177
Chandan <chandanbehera4598@gmail.com>
2026-07-17 21:22:48 +0530

    Q3

--- Logical_programing/Strong_Number.java
+++ Logical_programing/Strong_Number.java
@@ -1,52 +1,9 @@
 void main()
 {
-int ch=Integer.parseInt(IO.readln().trim());
-switch (ch) {
-    case 1->
-    {
-        double balance=Double.parseDouble(IO.readln());
-        Account a=new Account(balance);
-        IO.println(""+a.getBalance());
-
-    }
-    case 2->
-    {
-        double b=Double.parseDouble(IO.readln());
-        double r=Double.parseDouble(IO.readln());
-        SavingsAccount a=new SavingsAccount(b);
-        IO.println(""+a.getBalance());
-        IO.println(""+a.calculateInterest(r));
-
-    }
+new a();
 }
 
-}
-
-class Account
+class a
 {
-double balance;
-Account(double balance)
-{
-this.balance=balance;
-}
-
-double getBalance()
-{
-    return balance;
-}
-}
-
-class SavingsAccount extends Account
-{
-    SavingsAccount(double balance)
-    {
-super(balance);
-    }
-
-
-    double calculateInterest(double rate)
-    {
-        return balance * (rate / 100);
-    }
-}
-
+    
+} 
