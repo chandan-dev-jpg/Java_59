27abe8d233617d9c310f48577483938a489c0509
Chandan <chandanbehera4598@gmail.com>
2026-07-21 19:44:06 +0530

    static2

---
+++ Notes/Opps/Main.java
@@ -0,0 +1,23 @@
+void main()
+{
+    new Main();
+}
+/**
+ * Main
+ */
+ class Main {
+
+    
+     final static int a;
+     static 
+{
+    print();
+    a=100;
+    IO.println(a);
+}
+public static void print()
+{
+    IO.print(a);
+}
+}
+
