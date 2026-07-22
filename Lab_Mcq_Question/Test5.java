4979bfde6591e7439b9dab8feeb823b482fd79e5
Chandan <chandanbehera4598@gmail.com>
2026-07-22 18:25:47 +0530

    solve question

---
+++ Lab_Mcq_Question/Test5.java
@@ -0,0 +1,21 @@
+
+class StaticTrick9
+{
+    static StaticTrick9 obj;
+
+    static 
+    {
+        obj = new StaticTrick9();
+        System.out.println("Static block executed");
+    }
+
+    StaticTrick9()
+    {
+        System.out.println("Constructor executed");
+    }
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Main method");
+    }
+}
