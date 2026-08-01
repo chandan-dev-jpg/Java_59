28363e0fde351771788dfeffbb2563b1d46b004d
Chandan <chandanbehera4598@gmail.com>
2026-08-01 22:01:50 +0530

    pratice

--- Notes/Opps/Test.java
+++ Notes/Opps/Test.java
@@ -1,3 +1,23 @@
-public class Test {
-    
+void main()
+{
+Maneger m1=new Maneger(100,"Scott");
+Maneger m2=new Maneger(101,"Raju");
+m1.equals(m2);
+}
+
+class Maneger
+{
+    private int id;
+    private String name;
+    public Maneger(int id,String name)
+    {
+        this.name=name;
+        this.id=id;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+
+    }
 }
